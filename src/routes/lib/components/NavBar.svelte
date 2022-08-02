cf6201f01ef5c7d9220990bc6501a8372902f619
Vegar Skaret <vskaret@mykolab.com>
2022-08-02 23:31:32 +0200

    Tinker with mobile responsivity or something

--- src/routes/lib/components/NavBar.svelte
+++ src/routes/lib/components/NavBar.svelte
@@ -1,12 +1,18 @@
 <!--<div class="flex justify-around container bg-slate-700 mx-auto pt-4 pb-4 items-baseline min-w-full max-w-2xl">-->
 
 <div class="min-w-full bg-slate-700">
-	<!--class="grid grid-cols-3 grid-rows-1 justify-around container mx-auto pt-4 pb-4 items-baseline max-w-2xl"-->
-	<div class="grid grid-cols-8 items-baseline mx-auto py-4 max-w-2xl">
-		<div class="col-span-3 justify-self-start text-4xl text-white"><a href="/">travbula</a></div>
-		<div class="col-span-5 justify-self-end :grid grid-cols-3 text-lg text-white font-light">
-			a blog about nothing
-			<!--
+    <!--class="grid grid-cols-3 grid-rows-1 justify-around container mx-auto pt-4 pb-4 items-baseline max-w-2xl"-->
+    <div class="grid grid-cols-8 items-baseline mx-auto py-4 max-w-2xl px-2">
+        <div
+            class="col-span-3 justify-self-start text-2xl sm:text-4xl text-white"
+        >
+            <a href="/">travbula.com</a>
+        </div>
+        <div
+            class="col-span-5 justify-self-end :grid grid-cols-3 text-base sm:text-lg text-white font-light"
+        >
+            a blog about nothing
+            <!--
 			<a class="border-b-2 border-slate-700 hover:border-b-2 hover:border-white px-2" href="/filter"
 				>Filter</a
 			>
@@ -17,9 +23,9 @@
 				>About</a
 			>
 			-->
-		</div>
-		<div class="col-span-0 justify-self-end">
-			<!--
+        </div>
+        <div class="col-span-0 justify-self-end">
+            <!--
 			<input
 				class="text-white border border-slate-200 p-2 rounded-lg text-base placeholder-slate-700 "
 				type="text"
@@ -27,6 +33,6 @@
 				size="8"
 			/>
 			-->
-		</div>
-	</div>
+        </div>
+    </div>
 </div>
